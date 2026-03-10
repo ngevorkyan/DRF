d0ae2d282bd49175e09816aa9a8dcff30a664c24
ngevorkyan <gevorkyan.nino@gmail.com>
2026-03-10 21:59:44 +0400

    testing registration

--- snippets/views.py
+++ snippets/views.py
@@ -64,7 +64,12 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = [AllowAny]  # anyone can register
+    permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
